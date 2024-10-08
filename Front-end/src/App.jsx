3211d63321f52b3f56fda14194a1db4c350e5efa
--get <youssefsamirallam@gmail.com>
2024-10-08 11:50:46 +0300

    adding the latest fronted update to main

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./Components/Applayout";
 import Home from "./pages/Home";
 import Categories from "./pages/Categories";
-import Product from "./pages/Product";
+import Products from "./pages/Products";
 import ProductDetails from "./feature/Products/ProductDetails";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -22,7 +22,7 @@ function App() {
           <Route element={<AppLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/categories" element={<Categories />} />
-            <Route path="/product" element={<Product />} />
+            <Route path="/product" element={<Products />} />
             <Route path="/product/:id" element={<ProductDetails />} />
           </Route>
         </Routes>

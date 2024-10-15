4ce90b1a24de9272221e26600c51aa0ffa43579f
--get <youssefsamirallam@gmail.com>
2024-10-15 13:19:21 +0300

    fetching single product by query string Not req body

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -15,9 +15,13 @@ const addProduct = async (req, res) => {
     } = req.body;
 
     // Handle image files dynamically
-    const imageFiles = Object.values(req.files).flat().filter((file) => file);
+    const imageFiles = Object.values(req.files)
+      .flat()
+      .filter((file) => file);
     if (imageFiles.length === 0) {
-      return res.status(400).json({ success: false, message: "At least one image is required." });
+      return res
+        .status(400)
+        .json({ success: false, message: "At least one image is required." });
     }
 
     // Upload images to Cloudinary
@@ -44,12 +48,12 @@ const addProduct = async (req, res) => {
       category,
       subCategory,
       sizes: JSON.parse(sizes || "[]"), // Default to empty array if sizes is undefined
-      bestseller: bestseller === "true" ? true : false ,
+      bestseller: bestseller === "true" ? true : false,
       date: Date.now(),
     };
 
     console.log("Product data to save:", productData);
-    
+
     // Save the product
     const product = new productModel(productData);
     await product.save();
@@ -91,7 +95,7 @@ const removeProduct = async (req, res) => {
 // Function for single product info
 const singleProduct = async (req, res) => {
   try {
-    const { productId } = req.body;
+    const  productId  = req.query.productId;
     const product = await productModel.findById(productId);
     if (!product) {
       return res.status(404).json({ message: "Product not found." });

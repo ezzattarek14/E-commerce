5ca745e30371953d05152a564b9897a7ba446847
Mahmoud20301 <modymohamed1121@gmail.com>
2025-10-23 20:35:52 +0300

    dockerfile done

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/mongodb.js";
-import { connect } from "mongoose";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
@@ -10,30 +11,39 @@ import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import globaErrorController from "./controllers/globaErrorController.js";
 import notFoundPageError from "./routes/notFoundPageError.js";
-// App Config
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// MongoDB + Cloudinary
 connectDB();
 connectCloudinary();
 
-// MIDDLEWARES
-
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// api endpoints
+// API endpoints
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.send("API Working");
+// Serve frontend static files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+
+// React Router fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-//middlewares that we will assign any err to them using next(err);
-//it has to be the last used middleware
+
+// Error handlers
 app.use(notFoundPageError);
 app.use(globaErrorController);
 
 app.listen(port, () => console.log("server started on port :" + port));
+
